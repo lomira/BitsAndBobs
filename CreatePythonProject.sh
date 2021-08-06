0c97430f7d16e82cc9616ad6dbc7ae4e6c10a9c5
lomi <lomi@lomag.com>
2021-08-06 23:15:37 +0200

    adding template

--- CreatePythonProject.sh
+++ CreatePythonProject.sh
@@ -1,34 +1,7 @@
 #!/usr/bin/env zsh
-mkdir /home/lomi/Documents/PythonProject/$1
-cd /home/lomi/Documents/PythonProject/$1
+git init ~/Documents/PythonProject/$1
+cd ~/Documents/PythonProject/$1
 
-gh repo create $1 --private --confirm
-git init
+gh repo create --template lomira/python_template
 
-echo "# $1" >> README.md
-git add README.md
-
-
-lo=$( tr '[A-Z]' '[a-z]' <<< $1)
-
-mkdir data
-mkdir test
-mkdir $lo
-
-touch $lo/$lo.py
-git add $lo/$lo.py
-touch $lo/__init__.py
-git add $lo/__init__.py
-touch $lo/helpers.py
-git add $lo/helpers.py
-
-touch test/$lo_test.py
-git add test/$lo_test.py
-touch test/helpers_test.py
-git add test/helpers_test.py
-
-git commit -m "first commit"
-git branch -M master
-git remote add origin git@github.com:lomira/$1.git
-git push -u origin master
-code .
+git push --set-upstream origin main
