3a86028eddc1bf0b0ad58e43ff4a4a60ba063cbe
lomi <lomi@lomag.com>
2021-08-06 23:49:57 +0200

    changes

--- CreatePythonProject.sh
+++ CreatePythonProject.sh
@@ -3,4 +3,5 @@ git init /home/lomi/Documents/PythonProject/$1
 cd /home/lomi/Documents/PythonProject/$1
 gh repo create --template lomira/python_template
 git pull origin main
+mv module_name $1
 $SHELL
