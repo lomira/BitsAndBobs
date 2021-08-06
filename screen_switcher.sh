f69f80fb4bcc083e081c59130168136c6d0ce8aa
lomi <lomi@lomag.com>
2021-08-06 20:29:08 +0200

    Change from xrandr to nvidia

--- screen_switcher.sh
+++ screen_switcher.sh
@@ -6,5 +6,5 @@ if [[ "$nbScreen" == "3" ]] ; then
 fi
 
 if [[ "$nbScreen" == "2" ]] ; then
-    xrandr --output DP-4 --mode 1920x1080 --output DP-2 --primary --mode 1920x1080 --rate 144 --right-of DP-4 --output HDMI-0 --same-as DP-4
+    nvidia-settings --assign "CurrentMetaMode=DPY-6: nvidia-auto-select @1920x1080 +0+0 {ViewPortIn=1920x1080, ViewPortOut=1920x1080+0+0}, DPY-1: nvidia-auto-select @1920x1080 +0+0 {ViewPortIn=1920x1080, ViewPortOut=1920x1080+0+0}, DPY-4: nvidia-auto-select @2560x1440 +1920+0 {ViewPortIn=2560x1440, ViewPortOut=2560x1440+0+0}"
 fi
